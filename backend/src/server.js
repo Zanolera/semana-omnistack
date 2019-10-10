a8e7bb08af6f79460112a3ce1c6e6ef7585c80f1
Zanolera <marckzanol@gmail.com>
2019-10-09 23:55:26 -0300

    project completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,12 +4,40 @@ const routes = require('./routes');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-sqzef.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
+// em producao usar o banco redis, por ser simples e rapido
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+});
+
+//middleware //add funcionalidade
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+});
+
+// GET, POST, PUT, DELETE
+
+//req.query = Acessar query params (para filtros)
+//req.params = Acessar route params (para edição, delete)
+//req.body = Acessar corpo da requisição (para criação, edição)
 
 app.use(cors());
 app.use(express.json());
@@ -17,4 +45,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 
-app.listen(3333);
+server.listen(3333);
--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, ScrollView, AsyncStorage, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import socketio from 'socket.io-client';
+import { Alert, SafeAreaView, ScrollView, AsyncStorage, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 
 import SpotList from '../components/SpotList';
 
@@ -9,17 +10,25 @@ export default function List({ navigation }) {
     const [techs, setTechs] = useState([]);
 
     useEffect(() => {
-        AsyncStorage.getItem('techs').then( storagedTechs => {
+        AsyncStorage.getItem('user').then( user_id => {
+            const socket = socketio('http://192.168.0.11:3333', {
+                query: { user_id }
+            });
 
+            socket.on('booking_response', booking => {
+                Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved ? 'APROVADA' : 'REJEITADA'}`);
+            });
+        })
+    }, []);
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then( storagedTechs => {
             try{
                 const techsArray = storagedTechs.split(',').map(tech => tech.trim());
                 setTechs(techsArray);
             }catch{
                 console.log('Lista de tecnologias nao informadas.');
             }
-            
-
-            
         })
     }, []);
 
